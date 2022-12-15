1d14f30eb490e1e3a631652ac259fb3814adf0c4
ssmdssmp <dmitryy.volov@gmail.com>
2022-12-15 17:43:11 +0200

    try

--- src/hooks/userHooks.ts
+++ src/hooks/userHooks.ts
@@ -4,44 +4,42 @@ import { UserType, UserDataType, LoginDataType, PostType } from "../types";
 
 export const getUser = createAsyncThunk<UserDataType>(
   "social/getUser",
-  async (id) => {
-    return await axios.get(`/api/users/${id}`).then((res) => res.data);
+  (id) => {
+    return axios.get(`/api/users/${id}`).then((res) => res.data);
   }
 );
 export const login = createAsyncThunk<LoginDataType>(
   "social/login",
-  async (values) => {
-    return await axios.post("/api/auth/login", values).then((res) => res.data);
+  (values) => {
+    return axios.post("/api/auth/login", values).then((res) => res.data);
   }
 );
 export const registration = createAsyncThunk<LoginDataType>(
   "social/registration",
-  async (values) => {
-    return await axios
-      .post("api/auth/registration", values)
-      .then((res) => res.data);
+  (values) => {
+    return axios.post("api/auth/registration", values).then((res) => res.data);
   }
 );
 export const getUserPosts = createAsyncThunk<PostType[]>(
   "social/getUserPosts",
-  async ({ id, offset }: any) => {
-    return await axios
+  ({ id, offset }: any) => {
+    return axios
       .get(`/api/users/posts/${id}/${offset}`)
       .then((res) => res.data);
   }
 );
 export const followUser = createAsyncThunk<UserType[]>(
   "social/followUser",
-  async ({ userId, currentUserId }: any) => {
-    return await axios
+  ({ userId, currentUserId }: any) => {
+    return axios
       .put(`/api/users/${userId}/follow`, { userId: currentUserId })
       .then((res) => res.data);
   }
 );
 export const unfollowUser = createAsyncThunk<UserType[]>(
   "social/unfollowUser",
-  async ({ userId, currentUserId }: any) => {
-    return await axios
+  ({ userId, currentUserId }: any) => {
+    return axios
       .put(`/api/users/${userId}/unfollow`, { userId: currentUserId })
       .then((res) => res.data);
   }
